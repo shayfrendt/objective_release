229a5e6acbe06a424f6268ae4cf1417b93832c90
Shay Frendt <shay.frendt@gmail.com>
2011-07-06 16:01:00 -0400

    Refactoring fixtures out into separate fixture files

--- spec/objective_release/release_spec.rb
+++ spec/objective_release/release_spec.rb
@@ -3,46 +3,40 @@ require 'fileutils'
 
 module ObjectiveRelease
   describe Release do
+    let(:release_notes_start) { File.read(fixture_file_path("RELEASE_NOTES_START")) }
+    let(:release_notes_start) { File.read(fixture_file_path("RELEASE_NOTES_END")) }
+    let(:config_start) { File.read(fixture_file_path("Config-Start.plist")) }
+    let(:config_end) { File.read(fixture_file_path("Config-End.plist")) }
+
+    # context ".update_release_notes" do
+    #   before do
+    #     FileUtils.rm_rf(sample_release_notes_file)
+    #     test_file = File.open(sample_release_notes_file, 'w') do |f|
+    #       f.write(sample_release_notes)
+    #     end
+    #   end
+
+    #   it "includes the current app version number" do
+    #     release = Release.new
+    #     release.update_release_notes(sample_release_notes_file)
+    #     File.read(sample_release_notes).should == updated
+    #   end
+    # end
     
     context ".update_bundle_version" do
-      let(:sample_plist_file) { fixture_file_path("Test.plist") }
-      let(:sample_plist_contents) { <<-EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>CFBundleName</key>
-	<string>Objective Release Sample</string>
-	<key>CFBundleVersion</key>
-	<string>1.42</string>
-</dict>
-</plist>
-EOF
-}
-      let(:updated_plist_contents) { <<-EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>CFBundleName</key>
-	<string>Objective Release Sample</string>
-	<key>CFBundleVersion</key>
-	<string>1.43</string>
-</dict>
-</plist>
-EOF
-}
+      let(:config_test_file) { fixture_file_path("Config-Test.plist") }
+      
       before do
-        FileUtils.rm_rf(sample_plist_file)
-        test_file = File.open(sample_plist_file, 'w') do |f|
-          f.write(sample_plist_contents)
+        FileUtils.rm_rf(config_test_file)
+        test_file = File.open(config_test_file, 'w') do |f|
+          f.write(config_start)
         end
       end
      
       it "increments the minor version by 1" do
         release = Release.new
-        release.update_bundle_version(sample_plist_file)
-        File.read(sample_plist_file).should == updated_plist_contents
+        release.update_bundle_version(config_test_file)
+        File.read(config_test_file).should == config_end
       end
     end
 
